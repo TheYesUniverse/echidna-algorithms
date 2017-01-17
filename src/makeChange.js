7e9b86f63ee7eadc8043b3c79c669e4711945c31
Pheonix Ruachshaddai <pheonixruachshaddai@Pheonixs-MacBook-Pro.local>
2017-01-17 15:00:48 -0800

    solved makeChange

--- src/makeChange.js
+++ src/makeChange.js
@@ -1,3 +1,28 @@
 export default function makeChange({price, amountGiven}) {
-  // your code here
+  let change = {quarters:0, dimes:0, nickels:0, pennies:0
+  }
+
+let remainder = amountGiven-price
+
+if (remainder < 0 ){
+  return 'not enough money'
+}
+let quarters = Math.floor(remainder/25);
+change.quarters += quarters
+remainder = remainder - (quarters*25)
+
+let dimes = Math.floor(remainder/10);
+change.dimes += dimes
+remainder = remainder - (dimes*10)
+
+let nickels = Math.floor(remainder/5);
+change.nickels += nickels
+remainder = remainder - (nickels*5)
+
+let pennies = Math.floor(remainder)
+change.pennies = remainder
+
+
+return change
+
 }
