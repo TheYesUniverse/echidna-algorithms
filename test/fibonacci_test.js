08ed02fe0636f7dcac20f4a3af1d5a714ad6b6ac
Pheonix Ruachshaddai <pheonixruachshaddai@Pheonixs-MacBook-Pro.local>
2017-01-18 14:38:01 -0800

    solved fibonacci

--- test/fibonacci_test.js
+++ test/fibonacci_test.js
@@ -1,14 +1,15 @@
 import { expect } from 'chai'
-import Fibonacci from '../src/fibonacci'
+import fibonacci from '../src/fibonacci'
 
-describe.only('fibonacci', () => {
+describe('fibonacci', () => {
     'use strict'
   it('should be a function', () =>{
     expect(fibonacci).to.be.a('function');
   })
   it('should return the fibonacci sequence to n', () =>{
     expect(fibonacci(5)).to.deep.equal([0, 1, 1, 2, 3]);
-    expect(fibonacci(6)).to.equal([0, 1, 1, 2, 3, 5]);
-    expect(fibonacci(9)).to.equal([0, 1, 1, 2, 3, 5, 8, 13, 21]);
-    expect(fibonacci(10)).to.equal([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+    expect(fibonacci(6)).to.deep.equal([0, 1, 1, 2, 3, 5]);
+    expect(fibonacci(9)).to.deep.equal([0, 1, 1, 2, 3, 5, 8, 13, 21]);
+    expect(fibonacci(10)).to.deep.equal([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
   })
+})
