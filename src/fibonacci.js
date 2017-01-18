fb518b252f3f8439ebc647c38eac71aa047975c3
benjaminpettus <benjaminpettus@gmail.com>
2017-01-18 14:45:55 -0800

    changes

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -6,7 +6,8 @@ let array = [0, 1]
   if(n < 3) {
     return array
   }
-  for(let i = 2; i < n; i--){
-
+  for(let i = 2; i < n; i++){
+    array[i] = array[i - 2] + array[i -1]
   }
+  return array
 }
