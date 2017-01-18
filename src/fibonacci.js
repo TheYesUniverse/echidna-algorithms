f52c54ae981ee2ed80528bd695baa6126dde1be6
benjaminpettus <benjaminpettus@gmail.com>
2017-01-18 14:43:29 -0800

    changes

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,13 +1,12 @@
-export default function fibonacci (n) {
-let array =[0, 1]
-  if(n === 0) {
+export default function fibonacci(n) {
+let array = [0, 1]
+  if (n === 0){
     return false
   }
-  if(n < 3){
+  if(n < 3) {
     return array
   }
-  for(let i = 2; i < n; i++) {
-    array[i] = array[i-2] + array[i - 1]
+  for(let i = 2; i < n; i--){
+
   }
-  return array
 }
